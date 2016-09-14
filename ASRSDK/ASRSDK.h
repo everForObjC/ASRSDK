b3382368f1171e407e24d4346796a707e8414a9e
Mac <mac@macdemacbook-pro.local>
2016-09-14 15:09:07 +0800

    Initial Commit

---
+++ ASRSDK/ASRSDK.h
@@ -0,0 +1,19 @@
+//
+//  ASRSDK.h
+//  ASRSDK
+//
+//  Created by Mac on 16/9/14.
+//  Copyright © 2016年 kongzi2016. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ASRSDK.
+FOUNDATION_EXPORT double ASRSDKVersionNumber;
+
+//! Project version string for ASRSDK.
+FOUNDATION_EXPORT const unsigned char ASRSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASRSDK/PublicHeader.h>
+
+
